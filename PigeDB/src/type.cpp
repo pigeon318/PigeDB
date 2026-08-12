533df9df6c1631a649e17ec48a8fe4fa027b2893
pigeon318 <spark293984@gmail.com>
2026-08-12 21:25:24 +0100

    added more type cheks for integers and decimals

--- PigeDB/src/type.cpp
+++ PigeDB/src/type.cpp
@@ -1,12 +1,70 @@
 #include "../include/type.h"
 #include <iostream>
+#include <cctype>
 
 namespace pigedb {
 
 bool isBoolean(const std::string &value) {
     return value == "true" || value == "false";
 }
+bool isInteger(const std::string &value)
+{
+    if (value.empty()) {
+        return false;
+    }
 
+    std::size_t start = 0;
+    if (value[0] == '-' || value[0] == '+' ) {
+        start = 1;
+    }
 
+    if (start == value.size()) {
+        return false;
+    }
+
+    for (std::size_t i = start; i < value.size(); ++i)
+    {
+        if (!std::isdigit(static_cast<unsigned char>(value[i])))
+            return false;
+    }
+    return true;
 
 }
+bool isDecimal(const std::string &value) {
+    if (value.empty()) {
+        return false;
+    }
+    std::size_t start = 0;
+    if (value[0] == '-' || value[0] == '+') {
+        start = 1;
+    }
+
+    if (start == value.size()) {
+        return false;
+    }
+
+    bool hasDecimalPoint = false;
+
+    for (std::size_t i = start; i < value.size(); ++i)
+    {
+        if (value[i] == '.')
+        {
+            if (hasDecimalPoint) {
+                return false;
+            }
+
+            hasDecimalPoint = true;
+        }
+        else if (!std::isdigit(static_cast<unsigned char>(value[i])))
+        {
+            return false;
+        }
+    }
+
+    return hasDecimalPoint;
+}
+}
+
+
+
+
--- PigeDB/tests/typetest.cpp
+++ PigeDB/tests/typetest.cpp
@@ -2,7 +2,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << pigedb::isBoolean("true");
-    std::cout << pigedb::isBoolean("false");
-    std::cout << pigedb::isBoolean("hi");
+    std::cout << "is true a boolean " << pigedb::isBoolean("true") << "\n";
+    std::cout << "is false a boolean " << pigedb::isBoolean("false") << "\n";
+    std::cout << "is hi a boolean " << pigedb::isBoolean("hi") << "\n";
+    std::cout << "is 12345 a int " << pigedb::isInteger("12345") << "\n";
+    std::cout << "is true a int " << pigedb::isInteger("true") << "\n";
+    std::cout << "is 1.2 a decimal " << pigedb::isDecimal("1.2") << "\n";
+    std::cout << "is 1.23456 a decimal " << pigedb::isDecimal("1.23456") << "\n";
+    std::cout << "is hello a decimal " << pigedb::isDecimal("hello") << "\n";
+    std::cout << "is hello.1 a decimal " << pigedb::isDecimal("hello.1") << "\n";
+
 }
